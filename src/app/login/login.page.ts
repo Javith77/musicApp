f6f6b68307d43331099954d3beba421c233f7c3f
Javid Meneses <javimemo97@gmail.com>
2022-01-16 20:49:46 -0500

    Add: Login
    - Implementación de diseño login.
    - Imágenes agregadas.

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,17 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  login(): void {
+    this.router.navigateByUrl('/home');
+  }
+}
